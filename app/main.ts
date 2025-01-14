4af6cc290ba49ef28183da7484e1cda373c85bc8
GyeongChan <woig9874@naver.com>
2025-01-15 03:02:41 +0900

    refactor(main): improve event handling
    - 이벤트 핸들러 스타일 통일
    - 불필요한 코드 및 import 제거

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,4 @@
-import { ScriptPlayer } from "zep-script";
+import { ObjectEffectType, ScriptPlayer } from "zep-script";
 import { playerManager } from "./src/core/Player";
 import { environmentManager } from "./src/core/Environment";
 import { blockEntity } from "./src/entity/Block";
@@ -14,13 +14,14 @@ let _stateTimer = 0;
 let _transformCount = 0;
 let _answerCount = 0;
 
-ScriptApp.onInit.Add(() => {
+ScriptApp.onInit.Add(function () {
     environmentManager.initialize();
 });
 
-ScriptApp.onDestroy.Add(() => {
+ScriptApp.onDestroy.Add(function () {
     environmentManager.saveEnvironment();
     environmentManager.syncWithEnvironmentDB();
+    blockEntity.resetBlock();
 });
 
 // Q키를 눌렀을 때 블록 생성
@@ -33,18 +34,23 @@ ScriptApp.addOnKeyDown(82, function (player) {
     playerManager.toggleMovementMode(player); 
 });
 
-ScriptApp.onSidebarTouched.Add((player: ScriptPlayer) => {
+// 쓰레기를 때렸을 때 블록 제거
+ScriptApp.onAppObjectAttacked.Add(function (sender: ScriptPlayer, x: number, y: number, layer: number, key: string) {
+    blockEntity.attackedBlock(sender, key);
+});
+
+ScriptApp.onSidebarTouched.Add(function (player: ScriptPlayer) {
     const widget = player.showWidget("widget.html", "sidebar", 350, 350);
     environmentManager.setWidget(widget);
     player.tag.widget = widget;
 });
 
-ScriptApp.onUpdate.Add((dt: number) => {
+ScriptApp.onUpdate.Add(function (dt: number) {
     environmentManager.scheduleUpdateEnvironmentByMovement(dt);
     environmentManager.scheduleSaveEnvironment(dt);
 });
 
-ScriptApp.onLeavePlayer.Add((player: ScriptPlayer) => {
+ScriptApp.onLeavePlayer.Add(function (player: ScriptPlayer) {
     if (player.tag.widget) {
         player.tag.widget.destroy();
         player.tag.widget = null;
@@ -54,31 +60,28 @@ ScriptApp.onLeavePlayer.Add((player: ScriptPlayer) => {
     playerManager.syncWithPlayerDB(player);
 });
 
-ScriptApp.onJoinPlayer.Add((player: ScriptPlayer) => {
+ScriptApp.onJoinPlayer.Add(function (player: ScriptPlayer) {
     player.tag = {
         widget: null,
     };
-
-    const userExist = playerManager.loadPlayer(player);
-    if (!userExist) {
-        playerManager.initializePlayer(player);
-    }
-
+    
+    playerManager.loadPlayer(player);
 
     // 환경 지표 위젯 생성
-    const widget = player.showWidget("widget.html", "topleft", 300, 150);
+    const widget = {
+        environmentWidget: player.showWidget("widget.html", "topleft", 300, 150),
+        playerWidget: player.showWidget("info.html", "topright", 300, 150),
+    }
     if (widget) {
-        environmentManager.setWidget(widget);
+        environmentManager.setWidget(widget.environmentWidget);
+        playerManager.setWidget(widget.playerWidget);
     } else {
         ScriptApp.sayToStaffs(`[오류] ${player.name}님의 위젯 생성 실패`);
     }
 });
 
-ScriptApp.onSay.Add((player: ScriptPlayer, message: string) => {
+ScriptApp.onSay.Add(function (player: ScriptPlayer, message: string) {
     if(message === "scriptapp") {
         ScriptApp.sayToStaffs(ScriptApp.storage);
-    } else if (message === "resetentities") {
-        const entity = new Entity();
-        entity.resetEntities();
     }
 });
